77c5de061fae672ce25d6e9f6106341436402c94
Hamza Shahbaz <hamzashahbaz@live.com>
2020-06-28 08:09:05 -0700

    updates

--- docs/pages/components/layouts/button.js
+++ docs/pages/components/layouts/button.js
@@ -5,20 +5,20 @@ const Title = styled.h1`
     font-weight: 500;
     font-size: 24px;
     margin: 32px 0 8px;
-`
+`;
 const Subtitle = styled.p`
     font-weight: 300;
     font-size: 18px;
     margin: 0;
-    color: ${p => p.theme.colors.N300};
-`
+    color: ${(p) => p.theme.colors.N300};
+`;
 
-const Index = props => (
-        <Layout>
-            <p>Components / Button</p>
-            <Title>Buttons</Title>
-            <Subtitle>The classic button, in different colors, sizes, and states</Subtitle>
-        </Layout>
-)
+const Index = (props) => (
+    <Layout>
+        <p>Components / Button</p>
+        <Title>Buttons</Title>
+        <Subtitle>The classic button, in different colors, sizes, and states</Subtitle>
+    </Layout>
+);
 
 export default Index;
